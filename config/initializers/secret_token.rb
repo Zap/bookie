59629ef7d2ec4b25db1545f73c2fd002c14c51d8
Shawn Baker <zap@zapistan.net>
2010-12-26 15:15:42 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bookie::Application.config.secret_token = '02760000dbd41a6b0816e856ee2564872df09a7a2de2ad5188d32314874154e68963645648b4e01108a7184a15e7acd5126c1d04051037b7b9e3cee465e58dc7'
